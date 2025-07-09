62aa1a8734c1361797a0fdbf88b04ffb2afc9a71
Darshh16 <darshjilka09@gmail.com>
2025-07-09 09:54:28 +0530

    Secret key settings.py

--- currency_converter/settings.py
+++ currency_converter/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m+ua#!nu_h5(y4r$rybc974h&dqle^i%^)oxcyt2s!!4s71t@_'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
